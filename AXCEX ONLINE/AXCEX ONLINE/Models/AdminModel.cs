c22ff5488b25fd6faac6c9d4198c0cabd6fc98f3
MichaelTimbes <mtimbes@my.apsu.edu>
2018-02-25 23:16:45 -0600

    AXCEX

---
+++ AXCEX ONLINE/AXCEX ONLINE/Models/AdminModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AXCEX_ONLINE.Models
+{
+    public class AdminModel : ApplicationUser
+    {
+        public int ID { get; set; }
+        public string ADMIN_NAME { get; set; }
+
+    }
+}
---
+++ AXCEX ONLINE/AXCEX ONLINE/Models/EmployeeModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AXCEX_ONLINE.Models
+{
+    // Employee Model Inherits From Application User
+    public class EmployeeModel : ApplicationUser
+    {
+        public int ID { get; set; }
+        public string EMP_FNAME { get; set; }
+        public string EMP_LNAME { get; set; }
+
+    }
+}
